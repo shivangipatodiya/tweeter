6af7273b93c0528c9131c7f6fae1ddbd7d56cc2f
shivangipatodiya <shivu2610@gmail.com>
2021-10-06 23:31:08 +0000

    add edgecases for tweetlength before submit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,30 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: timeago.format(1461116232227)
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1461113959088
-  }
-];
+// const data = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac"
+//     },
+//     content: {
+//       text: "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     created_at: timeago.format(1461116232227)
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd"
+//     },
+//     content: {
+//       text: "Je pense , donc je suis"
+//     },
+//     created_at: 1461113959088
+//   }
+// ];
 
 //const $timePassed = timeago.format(new Date());
 
@@ -47,11 +47,21 @@ const renderTweets = (array) => {
 
 $("form").submit(function (event) {
   event.preventDefault();
+  let $tweetLength = $("#tweet-text").val().length;
+  if ($tweetLength === 0) {
+    alert("Tweet cannot be blank!");
+    return;
+  }
+  if ($tweetLength > 140) {
+    alert("Tweet is too long!");
+    return;
+  }
+  event.preventDefault();
   let $formInfo = $(this).serialize();
   $.ajax({
     url: "http://localhost:8080/tweets/",
     method: "POST",
-    data: $formInfo,
+    data: $formInfo
     //success: () => console.log($formInfo)
   });
 });
@@ -59,8 +69,10 @@ $("form").submit(function (event) {
 const loadTweets = () => {
   $.ajax({
     url: "http://localhost:8080/tweets/",
-    method: 'GET'
+    method: "GET"
   })
-  .then((result) => {renderTweets(result)})
-  .catch((error) => {})
-}
+    .then((result) => {
+      renderTweets(result);
+    })
+    .catch((error) => {});
+};
