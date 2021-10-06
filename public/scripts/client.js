3160801db9d8fee9e61bc1d46fc9650c46bd43fe
shivangipatodiya <shivu2610@gmail.com>
2021-10-06 22:56:01 +0000

    add form submit, use of ajax post and get

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,49 +6,61 @@
 
 const data = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac"
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1461116232227
+    created_at: timeago.format(1461116232227)
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd"
     },
-    "created_at": 1461113959088
+    content: {
+      text: "Je pense , donc je suis"
+    },
+    created_at: 1461113959088
   }
-]
+];
 
-//const $tweet = $(`<article class="tweet">Hello world</article>`);
-//console.log(tweetData.content.text);
-//const $tweet = createTweetElement(tweetData);
-
-
-
-const $timePassed = timeago.format(new Date());
+//const $timePassed = timeago.format(new Date());
 
 const createTweetElement = (data) => {
   let tweet = data.content.text;
-  return $(`<article class="tweet">${tweet}</article>`)
-}
+  return $(`<article class="tweet">${tweet}</article>`);
+};
 
 const renderTweets = (array) => {
   array.forEach((tweetObject) => {
-    console.log(tweetObject, '---------1')
-    let $tweet = createTweetElement(tweetObject)
-    console.log($tweet, '=========2')
+    let $tweet = createTweetElement(tweetObject);
     $(".tweet-container").append($tweet);
   });
 };
 
-renderTweets(data);
+//renderTweets(data);
+
+$("form").submit(function (event) {
+  event.preventDefault();
+  let $formInfo = $(this).serialize();
+  $.ajax({
+    url: "http://localhost:8080/tweets/",
+    method: "POST",
+    data: $formInfo,
+    //success: () => console.log($formInfo)
+  });
+});
+
+const loadTweets = () => {
+  $.ajax({
+    url: "http://localhost:8080/tweets/",
+    method: 'GET'
+  })
+  .then((result) => {renderTweets(result)})
+  .catch((error) => {})
+}
