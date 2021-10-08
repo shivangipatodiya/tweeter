2f2874debdaf72df51443fd4f5e828d48e986ddb
shivangipatodiya <shivu2610@gmail.com>
2021-10-08 20:30:19 +0000

    add responsive design

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,90 +4,113 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac"
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     created_at: timeago.format(1461116232227)
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd"
-//     },
-//     content: {
-//       text: "Je pense , donc je suis"
-//     },
-//     created_at: 1461113959088
-//   }
-// ];
+$(document).ready(function () {
+  /*
+   * Function to create a new tweet box every time a user creates a tweet.
+   */
 
-//const $timePassed = timeago.format(new Date());
+  const createTweetElement = function (data) {
+    let $userName = data.user.name;
+    let $handle = data.user.handle;
+    let $avatar = data.user.avatars;
+    let $text = data.content.text;
+    let $time = timeago.format(data.created_at);
+    let $tweetBox = $(`<div class="single-tweetbox"></div>`);
+    let $header = $(`<header class="list-name">
+                      <div>${$handle}</div>
+                    </header>`);
+    let $nameAvatar = $(`<div class="name-avatar"></div>`);
+    let image = $(`<img width="40px" alt="avatar" />`);
+    image.attr("src", $avatar);
+    $nameAvatar.append(image).append($(`<div>&nbsp;&nbsp;${$userName}</div>`));
+    $header.prepend($nameAvatar);
+    let $tweet = $("<article>").text($text);
+    let $footer = $(`<footer>
+                    <div>
+                      <div id="time">${$time}</div>
+                      <div class="icons">
+                        <i class="fa-solid fa-flag"></i>
+                        <i class="fa-solid fa-retweet"></i>
+                        <i class="fa-solid fa-heart"></i>
+                      </div>
+                    </div>
+                  </footer>`);
+    return $tweetBox.append($header).append($tweet).append($footer);
+  };
 
-const createTweetElement = (data) => {
-  let tweet = data.content.text;
-  let $text = $("<article>").text(data.content.text);
-  return $text.addClass("tweet");
-  //$(`<article class="tweet">${tweet}</article>`);
-};
+  /*
+   * Function to add the created tweet box to the tweets container
+   */
 
-const renderTweets = (array) => {
-  array.forEach((tweetObject) => {
-    let $tweet = createTweetElement(tweetObject);
-    $(".tweet-container").append($tweet);
-  });
-};
-
-//renderTweets(data);
-
-$("form").submit(function (event) {
-  event.preventDefault();
-  let $tweetLength = $("#tweet-text").val().length;
-  if ($tweetLength === 0) {
-    $("#error").slideDown("slow", () => {
-      $("#error").html(
-        `<i class="fa-solid fa-circle-xmark"></i>&nbsp;&nbsp;Tweet cannot be blank!`
-      );
-      $("#error").show();
+  const renderTweets = (array) => {
+    array.forEach((tweetObject) => {
+      let $tweet = createTweetElement(tweetObject);
+      $(".tweet-container").append($tweet);
     });
-    return;
-  }
-  if ($tweetLength > 140) {
-    $("#error").slideDown("slow", () => {
-      $("#error").html(
-        `<i class="fa-solid fa-circle-xmark"></i>&nbsp;&nbsp;Tweet is too long!`
-      );
-      $("#error").show();
-    });
-    return;
-  }
-  event.preventDefault();
-  let $formInfo = $(this).serialize();
-  $.ajax({
-    url: "http://localhost:8080/tweets/",
-    method: "POST",
-    data: $formInfo,
-    success: loadTweets()
-  });
-});
+  };
 
-const loadTweets = () => {
-  $("#error").slideUp("slow");
-  $.ajax({
-    url: "http://localhost:8080/tweets/",
-    method: "GET"
-  })
-    .then((result) => {
-      console.log("myresult", result);
-      renderTweets(result);
+  /*
+   * Event handling for submit button + check for apt tweetlength
+   * else error message show-up
+   */
+
+  $("form").submit(function (event) {
+    event.preventDefault();
+    let $tweetLength = $("#tweet-text").val().length;
+    if ($tweetLength === 0) {
+      $("#error").slideDown("slow", () => {
+        $("#error").html(
+          `<i class="fa-solid fa-circle-xmark"></i>&nbsp;&nbsp;Tweet cannot be blank!`
+        );
+        $("#error").show();
+      });
+      return;
+    }
+    if ($tweetLength > 140) {
+      $("#error").slideDown("slow", () => {
+        $("#error").html(
+          `<i class="fa-solid fa-circle-xmark"></i>&nbsp;&nbsp;Tweet is too long!`
+        );
+        $("#error").show();
+      });
+      return;
+    }
+    // create query string
+    let $formInfo = $(this).serialize();
+
+    // ajax post request
+    $.ajax({
+      url: "http://localhost:8080/tweets/",
+      method: "POST",
+      data: $formInfo,
+      success: () => {
+        $("#tweet-text").val("");
+        $(".counter").val(140);
+
+        //function call to get  and load the refreshed content of /tweets/ page
+        //without need to refresh
+        loadTweets();
+      }
+    });
+  });
+  /*
+   * Function to load the list of tweet boxes on the main page
+   * without the need to refresh and in a sorted manner
+   */
+  const loadTweets = () => {
+    $("#error").slideUp("slow");
+    $.ajax({
+      url: "http://localhost:8080/tweets/",
+      method: "GET"
     })
-    .catch((error) => {});
-};
-//loadTweets();
+      .then((result) => {
+        result.sort((a, b) => b.created_at - a.created_at);
+        $(".tweet-container").empty();
+        renderTweets(result);
+      })
+      .catch((error) => {
+        console.log("Error fetching tweets", error);
+      });
+  };
+  loadTweets();
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,17 @@
+/*
+ * This shows changing count of characters while writing a tweet
+ */
+
 $(document).ready(function () {
-  console.log("ready");
-});
-
-
-$("#tweet-text").keyup(function (e) {
-  let count = $(this).val().length;
-  let counterDiv = $(this).next().children("output.counter");
-  let remainingLetters = 140 - count;
-  counterDiv.val(remainingLetters);
-  if (remainingLetters >= 0) {
-    counterDiv.css("color", "#545149");
-  } else if (remainingLetters < 0) {
-    counterDiv.css("color", "red");
-  }
+  $("#tweet-text").keyup(function (e) {
+    let count = $(this).val().length;
+    let counterDiv = $(this).next().children("output.counter");
+    let remainingLetters = 140 - count;
+    counterDiv.val(remainingLetters);
+    if (remainingLetters >= 0) {
+      counterDiv.css("color", "#545149");
+    } else if (remainingLetters < 0) {
+      counterDiv.css("color", "red");
+    }
+  });
 });
